cb1ff621dd7b23c888b1ce7457a8cfb39517148e
SeongYongLee <seongyong.dev@gmail.com>
2022-10-15 00:45:41 +0900

    feat: react, next, emotion 추가

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <div>HOMEPARTY</div>
+      <div>초대장을 만들어보세요</div>
+    </>
+  )
+}
+
+export default Home
