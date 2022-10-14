aaf75af60778d2e76cd226978dd01b400bbc6864
SeongYongLee <seongyong.dev@gmail.com>
2022-10-15 01:28:39 +0900

    feat: eslint, prettier 설정

--- src/components/common/button.tsx
+++ src/components/common/button.tsx
@@ -1,26 +1,9 @@
-import styled from '@emotion/styled'
 import React from 'react'
+import styled from '@emotion/styled'
 import theme from 'styles/theme'
 
 const Button = styled.button`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 62px 54px;
-
-  & p {
-    font-family: SBAggroB;
-    font-weight: normal;
-    padding-top: 18px;
-    font-size: 32px;
-    line-height: 140%;
-    color: ${theme.colors.black};
-
-    & span {
-      font-family: SBAggroB;
-      font-size: 26px;
-    }
-  }
+  color: ${theme.colors.black};
 `
 
 const ButtonComponent = ({}) => {
