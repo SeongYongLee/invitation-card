a840e1359a6cf04eaa16efc09fee42a3966543de
SeongYong Lee <seongyong.dev@gmail.com>
2022-10-16 23:06:03 +0900

    feat: 프로젝트 초기 설정
    * feat: react, next, emotion 추가
    
    * feat: theme 추가, 절대경로 추가, scr 폴더 생성
    
    * feat: eslint, prettier 설정
    
    * feat: framer-motion 추가
    
    * feat: framer-motion 초기 세팅, routes 설정

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import Button from 'components/common/button'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <div>HOMEPARTY</div>
+      <div>초대장을 만들어보세요</div>
+      <Button>홈파티 초대장 만들기</Button>
+    </>
+  )
+}
+
+export default Home
---
+++ src/styles/theme.ts
@@ -0,0 +1,10 @@
+const theme = {
+  colors: {
+    red: '#FF5252',
+    green: '#69FF7A',
+    black: '#131313',
+    white: '#FFFFFF',
+  },
+}
+
+export default theme
