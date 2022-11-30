bf4461a4858595e92ca5fc81c435124765b97809
SeongYongLee <seongyong.dev@gmail.com>
2022-12-01 03:22:03 +0900

    feat: mypage guestbook

--- src/components/common/button.tsx
+++ src/components/common/button.tsx
@@ -4,33 +4,41 @@ import { motion } from 'framer-motion'
 import { fadeScaleVariant } from 'styles/motions'
 import theme from 'styles/theme'
 
-export interface Props extends HTMLAttributes<HTMLButtonElement> {
-  children?: React.ReactNode
-}
-
 const Wrapper = styled(motion.div)`
   display: flex;
   justify-content: center;
   align-items: center;
 `
 
-const StyledButton = styled(motion.button)`
+const StyledButton = styled(motion.button)<{ width?: number; height?: number }>`
   background-color: ${theme.colors.black};
   border-radius: 12px;
-  width: 100%;
-  height: 50px;
+  width: ${({ width }) => (width ? `${width}px` : '100%')};
+  height: ${({ height }) => (height ? `${height}px` : '100%')};
   margin: 0 16px;
   color: white;
+  font-family: 'Pretendard';
   font-style: normal;
   font-weight: 700;
   font-size: 16px;
   line-height: 130%;
 `
 
-export default function Button({ children, onClick }: Props) {
+export interface Props extends HTMLAttributes<HTMLButtonElement> {
+  children?: React.ReactNode
+  width?: number
+  height?: number
+}
+
+export default function Button({ children, onClick, width, height }: Props) {
   return (
-    <Wrapper>
-      <StyledButton variants={fadeScaleVariant} onClick={onClick}>
+    <Wrapper initial="initial" animate="animate" variants={fadeScaleVariant}>
+      <StyledButton
+        variants={fadeScaleVariant}
+        width={width}
+        height={height}
+        onClick={onClick}
+      >
         {children}
       </StyledButton>
     </Wrapper>
