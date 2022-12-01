3e99574decf6c1512d479dec3176ee76283151a4
SeongYong Lee <seongyong.dev@gmail.com>
2022-12-02 01:47:39 +0900

    feat: mypage/setting init (#11)

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -2,4 +2,7 @@ export const ROUTES = {
   HOME: '/',
   CREATE: '/create',
   MYPAGE: '/mypage',
+  SETTING: '/setting',
+  TERMS: '/setting/terms',
+  POLICY: '/setting/policy',
 }
--- src/pages/mypage.tsx
+++ src/pages/mypage.tsx
@@ -4,6 +4,9 @@ import router from 'next/router'
 import styled from '@emotion/styled'
 import Button from 'components/common/button'
 import { Title } from 'components/create/common/title'
+import Archive from 'components/mypage/archive'
+import GuestBook from 'components/mypage/guestbook'
+import { ROUTES } from 'constants/routes'
 import { motion } from 'framer-motion'
 import { fadeScaleVariant } from 'styles/motions'
 
@@ -142,116 +145,11 @@ const Tab = styled(motion.div)<{ active?: 'active' | 'inactive' }>`
   opacity: ${({ active }) => (active === 'active' ? 1 : 0.2)};
 `
 
-const ArchiveList = styled(motion.div)``
-
-const Archive = styled(motion.div)``
-
-const ArchiveLetter = styled(motion.div)`
+const ArchiveList = styled(motion.div)`
   display: flex;
-  justify-content: space-between;
-
-  background: #f7f8fa;
-  border-radius: 12px;
-  padding: 7.5px 12px;
-
-  font-family: 'Pretendard';
-  font-style: normal;
-  font-weight: 500;
-  font-size: 16px;
-  line-height: 130%;
-
-  letter-spacing: -0.03em;
-
-  color: #000000;
+  flex-direction: column;
+  gap: 30px;
 `
-
-const ArchiveLetterTitle = styled(motion.div)`
-  display: flex;
-`
-
-const ArchiveLetterIcon = styled(motion.div)`
-  margin: 0 9px 0 0;
-  width: 22px;
-  height: 16px;
-  background: #77aa77;
-`
-
-const ArchiveLetterArrayIcon = styled(motion.div)`
-  width: 20px;
-  height: 20px;
-  background: #77aa77;
-`
-
-const ArchiveImage = styled(motion.div)`
-  background: #aa7777;
-  margin: 10px 0 12px;
-
-  height: 212px;
-  border: 1px solid #f4f4f4;
-  border-radius: 12px;
-`
-
-const ArchiveTitle = styled(motion.div)`
-  font-family: 'Pretendard';
-  font-style: normal;
-  font-weight: 500;
-  font-size: 16px;
-  line-height: 130%;
-
-  letter-spacing: -0.03em;
-`
-
-const ArchiveDate = styled(motion.div)`
-  margin: 4px 0;
-  font-family: 'Pretendard';
-  font-style: normal;
-  font-weight: 400;
-  font-size: 13px;
-  line-height: 130%;
-
-  letter-spacing: -0.03em;
-
-  color: #000000;
-
-  opacity: 0.4;
-`
-
-const ArchiveActive = styled(motion.div)`
-  display: flex;
-  justify-content: right;
-  align-items: center;
-`
-
-const ArchiveClap = styled(motion.div)`
-  padding: 0 20px;
-  display: flex;
-  justify-content: right;
-  align-items: center;
-  gap: 9px;
-`
-
-const ArchiveClapIcon = styled(motion.div)`
-  width: 16px;
-  height: 16px;
-  background: #77aa77;
-`
-
-const ArchiveClapCount = styled(motion.div)``
-
-const ArchiveComment = styled(motion.div)`
-  display: flex;
-  justify-content: right;
-  align-items: center;
-  gap: 9px;
-`
-
-const ArchiveCommentIcon = styled(motion.div)`
-  width: 16px;
-  height: 16px;
-  background: #77aa77;
-`
-
-const ArchiveCommentCount = styled(motion.div)``
 
 const GuestBookList = styled(motion.div)`
   display: flex;
@@ -261,57 +159,13 @@ const GuestBookList = styled(motion.div)`
   gap: 13px 16px;
 `
 
-const GuestBook = styled(motion.div)`
-  width: 158px;
-  height: 182px;
-
-  background: #f4f4f4;
-`
-
-const GuestBookImage = styled(motion.div)`
-  margin: 14px;
-  width: 130px;
-  height: 130px;
-
-  background: #22ff22;
-`
-
-const GuestBookText = styled(motion.div)`
-  margin: 0 14px;
-  display: flex;
-  justify-content: space-between;
-`
-
-const GuestBookName = styled(motion.div)`
-  font-family: 'Pretendard';
-  font-style: normal;
-  font-weight: 700;
-  font-size: 15px;
-  line-height: 130%;
-
-  letter-spacing: -0.03em;
-
-  color: #000000;
-`
-
-const GuestBookDate = styled(motion.div)`
-  font-family: 'Pretendard';
-  font-style: normal;
-  font-weight: 400;
-  font-size: 13px;
-  line-height: 130%;
-
-  letter-spacing: -0.03em;
-
-  color: #000000;
-
-  opacity: 0.4;
-`
-
 const MyPage: NextPage = () => {
   const [tab, setTab] = useState(0)
-  const backPage = () => {
-    router.back()
+  const pushHome = () => {
+    router.push(ROUTES.HOME)
+  }
+  const pushSetting = () => {
+    router.push(ROUTES.SETTING)
   }
 
   return (
@@ -322,10 +176,10 @@ const MyPage: NextPage = () => {
       variants={fadeScaleVariant}
     >
       <Header>
-        <IconWrapper onClick={backPage} variants={fadeScaleVariant}>
+        <IconWrapper onClick={pushHome} variants={fadeScaleVariant}>
           {'<'}
         </IconWrapper>
-        <div>설정</div>
+        <div onClick={pushSetting}>설정</div>
       </Header>
       <Title text={'나의 랜선 홈'} horizontalmargin={0} />
       <Profile variants={fadeScaleVariant}>
@@ -339,7 +193,7 @@ const MyPage: NextPage = () => {
             <Email>dvsvp@kakao.com</Email>
           </Detail>
         </User>
-        {tab === 1 && (
+        {tab === 0 && (
           <ManageDetail variants={fadeScaleVariant}>
             <div>예정된 홈파티가 없어요</div>
             <ManagementText>{'초대장 관리 >'}</ManagementText>
@@ -369,65 +223,20 @@ const MyPage: NextPage = () => {
       </TabWrapper>
       {tab === 0 && (
         <ArchiveList variants={fadeScaleVariant}>
-          <Archive variants={fadeScaleVariant}>
-            <ArchiveLetter variants={fadeScaleVariant}>
-              <ArchiveLetterTitle>
-                <ArchiveLetterIcon />
-                꿈틀희네 러브러브 하우스
-              </ArchiveLetterTitle>
-              <ArchiveLetterArrayIcon />
-            </ArchiveLetter>
-            <ArchiveImage variants={fadeScaleVariant} />
-            <ArchiveTitle variants={fadeScaleVariant}>
-              초대장팀과 함께한 꿈틀희 홈파티
-            </ArchiveTitle>
-            <ArchiveDate variants={fadeScaleVariant}>
-              2022.10.10. 17:30
-            </ArchiveDate>
-            <ArchiveActive>
-              <ArchiveClap variants={fadeScaleVariant}>
-                <ArchiveClapIcon />
-                <ArchiveClapCount>14</ArchiveClapCount>
-              </ArchiveClap>
-              <ArchiveComment variants={fadeScaleVariant}>
-                <ArchiveCommentIcon />
-                <ArchiveCommentCount>7</ArchiveCommentCount>
-              </ArchiveComment>
-            </ArchiveActive>
-          </Archive>
-          <Archive></Archive>
+          <Archive />
+          <Archive />
         </ArchiveList>
       )}
       {tab === 1 && (
         <GuestBookList variants={fadeScaleVariant}>
-          <GuestBook variants={fadeScaleVariant}>
-            <GuestBookImage variants={fadeScaleVariant} />
-            <GuestBookText>
-              <GuestBookName>수진우주</GuestBookName>
-              <GuestBookDate>1일 전</GuestBookDate>
-            </GuestBookText>
-          </GuestBook>
-          <GuestBook variants={fadeScaleVariant}>
-            <GuestBookImage variants={fadeScaleVariant} />
-            <GuestBookText>
-              <GuestBookName>익명12</GuestBookName>
-              <GuestBookDate>1일 전</GuestBookDate>
-            </GuestBookText>
-          </GuestBook>
-          <GuestBook variants={fadeScaleVariant}>
-            <GuestBookImage variants={fadeScaleVariant} />
-            <GuestBookText>
-              <GuestBookName>종남</GuestBookName>
-              <GuestBookDate>1주 전</GuestBookDate>
-            </GuestBookText>
-          </GuestBook>
-          <GuestBook variants={fadeScaleVariant}>
-            <GuestBookImage variants={fadeScaleVariant} />
-            <GuestBookText>
-              <GuestBookName>꿈틀꿈틀</GuestBookName>
-              <GuestBookDate>3주 전</GuestBookDate>
-            </GuestBookText>
-          </GuestBook>
+          <GuestBook />
+          <GuestBook />
+          <GuestBook />
+          <GuestBook />
+          <GuestBook />
+          <GuestBook />
+          <GuestBook />
+          <GuestBook />
         </GuestBookList>
       )}
     </Content>
