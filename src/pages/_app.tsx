497b6e86c4a2d3ace7e61a1b6cd1b66d18dece68
SeongYong Lee <seongyong.dev@gmail.com>
2022-12-20 01:13:51 +0900

    feat: font add - lineseed (#14)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app'
 import Layout from 'styles/layout'
 
 import '../../public/fonts/pretendard/pretendard.css'
+import '../../public/fonts/lineseed/lineseed.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
