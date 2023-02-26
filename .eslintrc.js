a40eaa4e8e4baae6241fef018fcdac1e032a00c6
SeongYongLee <seongyong.dev@gmail.com>
2023-02-26 16:10:26 +0900

    feat: init storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,34 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
-  extends: ['plugin:react/recommended', 'next/core-web-vitals', 'prettier'],
+  extends: ['plugin:react/recommended', 'next/core-web-vitals', 'prettier', 'plugin:storybook/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 'latest',
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['react', '@typescript-eslint', 'simple-import-sort', 'unused-imports'],
   rules: {
     'require-jsdoc': 'off',
     'react/display-name': 'off',
     'simple-import-sort/exports': 'error',
-    'simple-import-sort/imports': [
-      'error',
-      {
-        groups: [
-          // react > next > @ > a~z
-          ['^react$', '^next', '^@', '^[a-z]'],
-          // ~
-          ['^~'],
-          // ./ > ../ > ../../
-          ['^\\./?$', '^\\.(?!/?$)', '^\\./(?=.*/)(?!/?$)', '^\\.\\./?$', '^\\.\\.(?!/?$)'],
-          // Side effect imports
-          ['^\\u0000'],
-        ],
-      },
-    ],
+    'simple-import-sort/imports': ['error', {
+      groups: [
+      // react > next > @ > a~z
+      ['^react$', '^next', '^@', '^[a-z]'],
+      // ~
+      ['^~'],
+      // ./ > ../ > ../../
+      ['^\\./?$', '^\\.(?!/?$)', '^\\./(?=.*/)(?!/?$)', '^\\.\\./?$', '^\\.\\.(?!/?$)'],
+      // Side effect imports
+      ['^\\u0000']]
+    }],
     'unused-imports/no-unused-imports-ts': 'error',
-    quotes: ['error', 'single'],
-  },
-}
+    quotes: ['error', 'single']
+  }
+};
