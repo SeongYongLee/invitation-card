c5c59598675c0dcba7f751681c1d8c2d64b7cd8c
SeongYongLee <seongyong.dev@gmail.com>
2023-03-30 14:25:31 +0900

    feat: storybook package.json 업데이트 반영

--- .storybook/main.js
+++ .storybook/main.js
@@ -14,6 +14,7 @@ module.exports = {
   core: {
     builder: '@storybook/builder-webpack5',
   },
+  staticDirs: ['../public'],
   webpackFinal: async (config) => {
     config.resolve.plugins = [new TsconfigPathsPlugin()]
     return config
