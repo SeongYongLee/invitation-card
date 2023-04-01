f449a18dbb8e9d6f8f4c951a5e4b160d02cf73ee
SeongYong Lee <seongyong.dev@gmail.com>
2023-04-01 21:12:10 +0900

    feat: storybook next.js 세팅 (#20)

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,8 @@
+import '../public/fonts/pretendard/pretendard.css'
+import '../public/fonts/lineseed/lineseed.css'
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
