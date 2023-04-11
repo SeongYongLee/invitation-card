aa919486f1267cc8b0c91f248f438591afe15d20
SeongYongLee <seongyong.dev@gmail.com>
2023-04-11 15:33:43 +0900

    feat: fabric package 추가 및 반영

--- src/components/create/third.tsx
+++ src/components/create/third.tsx
@@ -1,12 +1,19 @@
 import Input from 'components/common/Input'
+import Radio from 'components/common/radio'
 import { motion } from 'framer-motion'
 import { fadeScaleVariant } from 'styles/motions'
 
 import { Title } from './common/title'
 
+const values = ['집들이', '생일', '이벤트', '직접입력']
+
 export const CreateThird = () => {
   return (
     <motion.div initial="initial" animate="animate" variants={fadeScaleVariant}>
+      <Title text={'어떤 홈파티인가요?'} />
+      <Radio name={'type'} values={values} defaultValueIndex={1} />
+      <Title text={'게스트에게 전달할 내용'} />
+      <Input placeholder="파티 전달사항을 작성해보세요." />
       <Title text={'홈파티 제목을 지어주세요'} />
       <Input placeholder="예시) 불금의 파자마 파티" />
       <Title text={'장소는 어디인가요?'} />{' '}
