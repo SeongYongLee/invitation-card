c7d2828b8289b6f2324adad9bb21652f0ddbd1f1
SeongYongLee <seongyong.dev@gmail.com>
2023-04-11 15:33:43 +0900

    feat: fabric package 추가 및 반영

--- src/components/create/second.tsx
+++ src/components/create/second.tsx
@@ -1,19 +1,41 @@
-import Input from 'components/common/Input'
-import Radio from 'components/common/radio'
+import { useCallback } from 'react'
+import * as fabric from 'fabric'
 import { motion } from 'framer-motion'
 import { fadeScaleVariant } from 'styles/motions'
-
-import { Title } from './common/title'
-
-const values = ['집들이', '생일', '이벤트', '직접입력']
+import theme from 'styles/theme'
+import { Canvas } from 'utils/canvas'
 
 export const CreateSecond = () => {
+  const onLoad = useCallback(async (canvas: fabric.Canvas) => {
+    canvas.setDimensions({
+      width: window.innerWidth - 34,
+      height: 500,
+    })
+    const rect = new fabric.Rect({
+      top: 100,
+      left: window.innerWidth - 140,
+      width: 50,
+      height: 50,
+      fill: theme.colors.primary,
+    })
+    const circle = new fabric.Circle({
+      top: 170,
+      left: window.innerWidth - 100,
+      radius: 10,
+      fill: theme.colors.secondary,
+    })
+    const text = new fabric.Textbox('TODO: Create Custom ImageEditor', {
+      originX: 'center',
+      top: 100,
+      fontSize: 30,
+    })
+    canvas.add(rect, circle, text)
+    canvas.centerObjectH(text)
+  }, [])
+
   return (
     <motion.div initial="initial" animate="animate" variants={fadeScaleVariant}>
-      <Title text={'어떤 홈파티인가요?'} />
-      <Radio name={'type'} values={values} defaultValueIndex={1} />
-      <Title text={'게스트에게 전달할 내용'} />
-      <Input placeholder="파티 전달사항을 작성해보세요." />
+      <Canvas onLoad={onLoad} />
     </motion.div>
   )
 }
